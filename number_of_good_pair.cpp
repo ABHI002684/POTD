90ad07f8975bcd20cac5a71bc0600451e5e609f3
ABHI002684 <abhishek002684@gmail.com>
2024-01-02 23:03:05 +0530

    removing some error

--- number_of_good_pair.cpp
+++ number_of_good_pair.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int gpair=0,n=nums.size();
+        int goodpair=0,n=nums.size();
         for(int i=0;i<n-1;i++)
         {
             for(int j=i+1;j<n;j++)
             {
             if(nums[i]==nums[j] && i<j)
             {
-                gpair++;
+                goodpair++;
             }
             }
         }
-        return gpair;
+        return goodpair;
     }
 };
