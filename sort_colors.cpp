80b8dcdd6429a661d017835e8d84b4c5ef0a1132
ABHI002684 <abhishek002684@gmail.com>
2024-01-25 20:09:58 +0530

    adding answer

---
+++ sort_colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0=0,count1=0,count2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0) count0++;
+            else if(nums[i]==1) count1++;
+            else count2++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(i<count0) nums[i]=0;
+            else if(i<count1+count0) nums[i]=1;
+            else nums[i]=2;
+        }
+        
+        
+    }
+};
